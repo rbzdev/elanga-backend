7127683bc692dae9a49fd64798074a123d506173
rbzdev <rubuzolivier2@gmail.com>
2024-07-24 04:10:28 +0100

    Backend operationnel elanga

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const { createServer } = require('http');
-const app = require('../app'); // Assurez-vous que le chemin est correct
-
-const server = createServer(app);
-
-module.exports = (req, res) => {
-  server.emit('request', req, res);
-};
